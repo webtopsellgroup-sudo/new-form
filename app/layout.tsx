3a8c2e092b77d0349c6032cd733cd843f556b423
webtopsellgroup-sudo <web.topsellgroup@gmail.com>
2025-08-12 14:12:02 +0700

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Form Konfirmasi DP',
+  description: 'Created with Andi Susato',
+  generator: 'V0 x Andi',
 }
 
 export default function RootLayout({
