793c056329a6d8d238278abbf0265c55a3095c97
webtopsellgroup-sudo <web.topsellgroup@gmail.com>
2025-08-12 16:02:31 +0700

    update desk

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Form Konfirmasi Transfer',
+  description: 'Created by Andi Susanto',
   generator: 'v0.dev',
 }
 
